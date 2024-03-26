a8d6d415b1daf678b4d6d1429d59be45c334ec83
georgekrissak <georgekrissak@gmail.com>
2024-03-26 13:39:54 +0100

    hope this are last merge conflict changes

--- main.py
+++ main.py
@@ -5,18 +5,29 @@ app = FastAPI()
 
 from datetime import datetime, timedelta, timezone
 from typing import Annotated, Union
-
+import uvicorn
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 
+import crud, models, schemas
+from database import SessionLocal, engine
+
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "0d0be28f03ab10c4419e528e138d7495ae7f1c4ba8b00116d275361c35824783"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+models.Base.metadata.create_all(bind=engine)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
 
 
 fake_users_db = {
@@ -161,3 +172,23 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(db: Session = Depends(get_db)):
+    users = crud.get_users(db)
+    return users
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
