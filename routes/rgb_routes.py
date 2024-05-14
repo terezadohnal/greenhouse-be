bf87e86ca0c45d2910f8f85d60f1364df4b45496
georgekrissak <georgekrissak@gmail.com>
2024-05-14 14:40:42 +0200

    shows num of measurements in Home page

--- routes/rgb_routes.py
+++ routes/rgb_routes.py
@@ -7,10 +7,15 @@ from fastapi import Depends, HTTPException
 from typing import Annotated
 from fastapi.security import OAuth2PasswordBearer
 from rgb.rgb_controller import RGB
+import os
+from fastapi.responses import FileResponse
+
 
 rgb_router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+rgb_output_folder = "./rgb/output/"
 
 def get_db():
     db = SessionLocal()
@@ -30,3 +35,24 @@ def Connect():
 @rgb_router.post("/rgb/capturefake")
 def Connect():
     return RGB.captureFakeImage()
+
+
+@rgb_router.get("/rgb-photos/{photo_filename}")
+async def get_activity_photo(photo_filename: str):
+    photo_path = str(rgb_output_folder) + str(photo_filename)
+
+    if os.path.isfile(photo_path):
+        return FileResponse(photo_path)
+    else:
+        raise HTTPException(status_code=404, detail="Photo not found")
+
+
+@rgb_router.get("/rgb-photos/how-many/")
+async def get_activity_photo():
+    try:
+        photos = os.listdir(rgb_output_folder)
+        print(photos)
+        return photos
+    except:
+        return 0
+
