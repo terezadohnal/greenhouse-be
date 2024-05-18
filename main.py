9f9e53722acabef9bfb5772758008ba141b18c1d
Eliška Chylíková <xchyliko@mendelu.cz>
2024-05-18 10:31:10 +0200

    Comment

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import uvicorn
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from models import user_model
-from database import engine
-from routes.user_routes import user_api_router
-from routes.camera_routes import camera_api_router
+# from models import user_model
+# from database import engine
+# from routes.user_routes import user_api_router
+# from routes.camera_routes import camera_api_router
 # from routes.rgb_routes import rgb_router
 
-user_model.Base.metadata.create_all(bind=engine)
+# user_model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-app.include_router(user_api_router)
-app.include_router(camera_api_router)
+# app.include_router(user_api_router)
+# app.include_router(camera_api_router)
 # app.include_router(rgb_router)
 
 origins = ["*"]
