aff23ae501af2b9a1b1262154e1efd79bdbd057e
Eliška Chylíková <xchyliko@mendelu.cz>
2024-05-18 10:39:02 +0200

    Comment

--- main.py
+++ main.py
@@ -29,5 +29,5 @@ async def root():
     return {"message": "Hello World"}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=3000)
