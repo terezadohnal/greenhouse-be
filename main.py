5fa0129b82279c21831155684f61f1acfd6dfac4
Eliška Chylíková <xchyliko@mendelu.cz>
2024-05-18 10:21:02 +0200

    Comment

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import uvicorn
 
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models import user_model
-from database import  engine
+from database import engine
 from routes.user_routes import user_api_router
 from routes.camera_routes import camera_api_router
-from routes.rgb_routes import rgb_router
+# from routes.rgb_routes import rgb_router
 
 user_model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(user_api_router)
 app.include_router(camera_api_router)
-app.include_router(rgb_router)
+# app.include_router(rgb_router)
 
 origins = ["*"]
 
